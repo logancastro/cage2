eb4b9577d0d6fe7d0af43850217b631f57320450
nestor.castro.depablos@gmail.com <>
2020-03-18 15:41:44 -0700

    ApexSQL Diff synchronization

---
+++ Users/BALBOA@Apex2.sql
@@ -0,0 +1,7 @@
+﻿-- Create user [BALBOA\Apex2]
+Print 'Create user [BALBOA\Apex2]'
+GO
+CREATE USER [BALBOA\Apex2]
+	FOR LOGIN [BALBOA\Apex2]
+	WITH DEFAULT_SCHEMA = [dbo]
+GO
---
+++ Users/BALBOA@Apex3.sql
@@ -0,0 +1,7 @@
+﻿-- Create user [BALBOA\Apex3]
+Print 'Create user [BALBOA\Apex3]'
+GO
+CREATE USER [BALBOA\Apex3]
+	FOR LOGIN [BALBOA\Apex3]
+	WITH DEFAULT_SCHEMA = [dbo]
+GO
